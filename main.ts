ba402da10182c66f49e55bad79ceb1af24fde6d2
Trevor <trevor@hauter.dev>
2025-05-25 21:19:06 -0400

    Update git ignore, add non-working clipboard reading logic

--- main.ts
+++ main.ts
@@ -1,14 +1,33 @@
-import { Notice, Plugin } from 'obsidian';
+import { EditorTransaction, Plugin, MarkdownView } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 export default class MyPlugin extends Plugin {
 
-	clipboardImageToMarkdown() {
-		new Notice('This is a notice!');
+	async clipboardImageToMarkdown() {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const clipboardText: ClipboardItems = await navigator.clipboard.read();
+		const output = '';
+
+		clipboardText.forEach(
+			(item) => {
+				item.items.forEach(singleItem => {
+					output.concat(singleItem);	
+				});
+			}
+		)
+
+		// Make sure the user is editing a Markdown file.
+		if (view) {
+			const transaction: EditorTransaction = {
+				replaceSelection: output,
+			};
+			view.editor.transaction(transaction);
+		}
 	}
 
 	onload() {
+
 		this.addCommand(
 			{
 				id: 'image-to-markdown',
